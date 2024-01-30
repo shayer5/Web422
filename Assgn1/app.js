fc5068640b4bf07dd0037702f5439150ecfcfdc2
shayer5 <shayer5@myseneca.ca>
2024-01-30 15:55:27 -0500

    Update app.js

--- Assgn1/app.js
+++ Assgn1/app.js
@@ -36,8 +36,8 @@ app.get('/', (req, res) => {
 
 // Get all
 app.get('/api/listings', (req, res) => {
-  const { page, perPage, name } = req.query;
-  db.getAllListings(page = 1,perPage = 15, name = '').then((listings) => {
+  const { page = 1, perPage = 15, name  = ''} = req.query;
+  db.getAllListings(page,perPage, name).then((listings) => {
     res.status(200).json(listings);
   }).catch((err) => {
     res.status(500).json({ message: `unable to get listings`, error: err });
