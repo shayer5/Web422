93219c0a4cccf7cb0fcb1d759094fc824e33b332
shayer5 <shayer5@myseneca.ca>
2024-02-01 20:33:50 -0500

    Update 1.0.1

--- Assgn2/js/main.js
+++ Assgn2/js/main.js
@@ -1,14 +1,39 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+// add variable and functions
+// page (number) - keeps track of which page the user is on (default: 1)
+var page = 1;
+// perPage (number) - the number of results to show per page (default: 10)
+var perPage = 10;
+// searchName (string) - keeps track of current search term (default: '')
+var searchName = '';
+//loadListingsData() (function)
+    //- pull listings from api
+    // format and add it to DOM
+    //add "click" event listener for each row item, which will show the modal window with the listing details
+function loadListingsData() {
+    //url to fetch data from
+    var url = `https://lucky-blue-cuttlefish.cyclic.app`;
+    fetch(`${url}/api/listings?page=${page}&perPage=${perPage}&name=${encodeURIComponent(searchName)}`)
+    .then(res => {
+        return res.ok ? res.json() : Promise.reject(res.status);
+    })
+    .then(data => {
+        if(data.length){
+            // non-empty array (listings available)
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
+        }else{
+            // no more listings available
+            if (page > 1) {
+                page--;
+            }
+            else {
+                //add text saying no listings available
+                var noListings = document.getElementById("listingsTable").getElementsByTagName("noListings");
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
+                noListings.innerHTML = `<tr><td colspan="4"><strong>No data available</td></tr>`;
+            }
+        }
+    }).catch(err => {
+        // error (no listings available)
+    });
+}
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
